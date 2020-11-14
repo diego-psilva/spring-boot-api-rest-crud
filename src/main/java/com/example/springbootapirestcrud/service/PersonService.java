7b64923f60deae0585c7504aa065d7d703e730cd
Diego PS <diegops.tads@gmail.com>
2020-11-14 15:44:29 -0300

    Create method

--- src/main/java/com/example/springbootapirestcrud/service/PersonService.java
+++ src/main/java/com/example/springbootapirestcrud/service/PersonService.java
@@ -12,34 +12,34 @@ public class PersonService {
 
     private final AtomicLong counter = new AtomicLong();
 
-    public Person findById(String id){
-
-        Person person = new Person();
-        person.setId(counter.incrementAndGet());
-        person.setFirstName("Phill");
-        person.setLastName("Collin");
-        person.setAdress("Hall Street, New York, USA");
-        person.setGender("male");
-
-        return person;
-    }
-
     public List<Person> findAll (){
-
         List<Person> people = new ArrayList<>();
-
         for(int i = 0; i<9; i++){
-
             Person person = new Person();
             person.setId(counter.incrementAndGet());
             person.setFirstName("Phill "+i);
             person.setLastName("Collin "+i);
             person.setAdress("Hall Street "+ i +", New York, USA");
             person.setGender("male");
-
             people.add(person);
         }
-
         return people;
     }
+
+    public Person findById(String id){
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Phill");
+        person.setLastName("Collin");
+        person.setAdress("Hall Street, New York, USA");
+        person.setGender("male");
+        return person;
+    }
+
+    public Person create (Person person){
+        person.setId(counter.incrementAndGet());
+        return person;
+    }
+
+
 }
