e0225fa6e34a7f447ed453c78cb9a17e4092fbf0
Diego PS <diegops.tads@gmail.com>
2020-11-14 12:54:16 -0300

    Initial commit

---
+++ src/main/java/com/example/springbootapirestcrud/SpringBootApiRestCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootapirestcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiRestCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiRestCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootapirestcrud/SpringBootApiRestCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootapirestcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApiRestCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
